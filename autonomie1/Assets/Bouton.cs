fc565f49b62ed0ec027aaae0d78414e653f50007
Llleea <premont.lea@gmail.com>
2021-02-11 11:54:25 +0100

    debut collab
    ici commence le projet

---
+++ autonomie1/Assets/Bouton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouton : MonoBehaviour
+{
+    float hauteur = 1.5f;
+    public GameObject cubeNouveau;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeNouveau = GameObject.Find("CubeApparait");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Sphere")
+        {
+            //transform.Translate(0, 10f, 0, Space.World);
+            Instantiate(cubeNouveau, new Vector3(4f, hauteur, -25f), Quaternion.identity);
+            hauteur += 3;
+        }
+    }
+}
+
+
---
+++ autonomie1/Assets/ScriptBouleDebloquer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptBouleDebloquer : MonoBehaviour
+{
+    bool temoin = false;
+    float vitesse = 0.02f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && temoin )
+        {
+            temoin = false;
+            transform.Translate(0, 1000 * Time.deltaTime, 0, Space.World);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(-vitesse, 0, 0, Space.World);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(vitesse, 0, 0, Space.World);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0, 0, -vitesse, Space.World);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0, 0, +vitesse, Space.World);
+        }
+    }
+
+
+        void OnMouseDown()
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+   void OnCollisionEnter(Collision collision)
+    {
+        
+        temoin = true;
+    }
+}
+
